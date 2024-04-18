ebb2a57d4f3b017e4869e2729569c8c5c49b4384
deekast <96024516+deekast@users.noreply.github.com>
2024-04-17 20:24:55 -0400

    lesson submit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,15 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 function NavBar() {
   return (
     <nav className="navbar">
-    
+      <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink>
+      <NavLink to="/directors" className="nav-link" activeClassName="active">Directors</NavLink>
+      <NavLink to="/actors" className="nav-link" activeClassName="active">Actors</NavLink>
     </nav>
-    );
+  );
 };
 
 export default NavBar;
